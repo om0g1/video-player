21f699120207a8dea28bbe6d3f167463cb8ebda0
om0g1 <omogidavis@gmail.com>
2023-07-19 02:27:45 +0300

    v0.0.1

--- main.js
+++ main.js
@@ -11,20 +11,120 @@ const fastFoward = document.getElementById("fast-foward");
 const skipEnd = document.getElementById("skip-end");
 const playSvg = document.getElementById("play-svg");
 const pauseSvg = document.getElementById("pause-svg");
-const volumeWrapper = document.getElementById("volume-wrapper");
-const volumeSlider = document.getElementById("volume-slider");
-let hasVideo = true;
+const pipBtn = document.getElementById("pip-btn");
+const captionsBtn = document.getElementById("captions-btn");
+const playbackBtn = document.getElementById("playback-btn");
+const fullscreenBtn = document.getElementById("fullscreen-btn");
+const volumeBtn = document.getElementById("volume-btn");
+const muteSvg = document.getElementById("mute-svg");
+const lowVolumeSvg = document.getElementById("volume-low-svg");
+const loudVolumeSvg = document.getElementById("volume-loud-svg");
+const volumeSlider = document.getElementById("volumeSlider");
+const infoBtn = document.getElementById("info-btn");
+const infoPopup = document.getElementById("info-popup");
+const closeInfo = document.getElementById("close-info-popup");
+const titleP = document.getElementById("title-p");
+const extensionP = document.getElementById("extension-p");
+const typeP = document.getElementById("type-p");
+const sizeP = document.getElementById("size-p");
+const dateP = document.getElementById("date-p");
+const durationP = document.getElementById("duration-p");
+const captions = video.textTracks[0];
+let hasVideo = false;
+let isMute = false;
 let isPlaying = false;
 let isOnSlider = false;
+let isPictureInPicture = false;
 let activeDropdown = null;
-let volume = 0.2;
+let volume = 1;
+
+
+function togglePip() {
+    if (isPictureInPicture) {
+         document.exitPictureInPicture();
+         isPictureInPicture = false;
+    } else {
+         video.requestPictureInPicture();
+         isPictureInPicture = true;
+    }
+ }
+ 
+ function togglePlay() {
+    if (video.src == ""){
+        alert("Please select a item to be played first");
+    } else {
+        if (isPlaying == false){
+            isPlaying = true;
+            video.play();
+            playSvg.style.display = "none";
+            pauseSvg.style.display = "block";
+        } else {
+            isPlaying = false;
+            video.pause();
+            pauseSvg.style.display = "none";
+            playSvg.style.display = "block";
+        }
+    }
+}
+
+function toggleFullscreen() {
+    if (document.fullscreenElement != null) {
+        document.exitFullscreen();
+    } else {
+        video.requestFullscreen();
+    }
+}
+
+function resetSlider(){
+    slider.min = 0;
+    slider.value = 0;
+    slider.max = video.duration;
+}
+
+function convertToTimeFormat(seconds){
+    let hours = 0;
+    let minutes =  0;
+    
+    while (seconds > 59) {
+        minutes += 1;
+        seconds -= 60;
+    }
+    while (minutes > 59) {
+        hours += 1;
+    }
+    
+    hours = Math.round(hours);
+    minutes = Math.round(minutes);
+    seconds = Math.round(seconds);
+
+    if (hours < 1) {
+        hours = "";
+    } else if (hours < 10) {
+        hours = "0" + hours + ":";
+    } else {
+        hours = hours + ":"
+    }
+    if (minutes < 1) {
+        minutes = "00:";
+    } else if (minutes < 10) {
+        minutes = "0" + minutes + ":";
+    } else {
+        minutes = minutes + ":";
+    }
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    return hours + minutes  + seconds;
+}
 
 window.onload = e => {
     pauseSvg.style.display = "none";
-    slider.min = 0;
-    slider.max = video.duration;
-    slider.value = 0;
+    resetSlider();
     activeDropdown = null;
+    loudVolumeSvg.style.display = "block";
+    slider.value = 0;
+    volumeSlider.value = 100;
+    hasVideo = false;
 }
 
 window.onclick = e => { 
@@ -37,10 +137,36 @@ window.onclick = e => {
     }
 }
 
+window.onkeyup = e => {
+    switch(e.key.toLowerCase()) {
+        case " ":
+        case "k":
+            togglePlay();
+            break;
+        case "f":
+            toggleFullscreen();
+            break;
+        case "p":
+            togglePip();
+            break;
+    }
+}
+
 openFile.onchange = e => {
     const file = e.target.files[0];
     video.src = URL.createObjectURL(file);
     hasVideo = true;
+    const name = file.name.split(".");
+    titleP.value = name[0];
+    extensionP.value = `.${name[name.length -1]}`
+    dateP.value = file.lastModifiedDate;
+    sizeP.value = `${(file.size / 102400).toFixed(2)}mb`;
+    typeP.value = file.type;
+    resetSlider();
+}
+
+video.onloadedmetadata = e => {
+    durationP.value = convertToTimeFormat(video.duration);
 }
 
 dropdownBtns.forEach((button) => {
@@ -86,17 +212,7 @@ reverse.onclick = e => {
 } 
 
 playPause.onclick = e => {
-    if (isPlaying == false){
-        isPlaying = true;
-        video.play();
-        playSvg.style.display = "none";
-        pauseSvg.style.display = "block";
-    } else {
-        isPlaying = false;
-        video.pause();
-        pauseSvg.style.display = "none";
-        playSvg.style.display = "block";
-    }
+    togglePlay();
 }
 
 fastFoward.onclick = e => {
@@ -124,53 +240,79 @@ setInterval(() => {
             }
         }, 1)
 
-function convertToTimeFormat(seconds){
-    let hours = 0;
-    let minutes =  0;
-    
-    while (seconds > 59) {
-        minutes += 1;
-        seconds -= 60;
-    }
-    while (minutes > 59) {
-        hours += 1;
-    }
-    
-    hours = Math.round(hours);
-    minutes = Math.round(minutes);
-    seconds = Math.round(seconds);
-
-    if (hours < 1) {
-        hours = "";
-    } else if (hours < 10) {
-        hours = "0" + hours + ":";
-    } else {
-        hours = hours + ":"
-    }
-    if (minutes < 1) {
-        minutes = "00:";
-    } else if (minutes < 10) {
-        minutes = "0" + minutes + ":";
-    } else {
-        minutes = minutes + ":";
-    }
-    if (seconds < 10){
-        seconds = "0" + seconds;
-    }
-    return hours + minutes  + seconds;
-}
-
 setInterval(() => {
     if (hasVideo){
         timeElapsed.innerHTML = convertToTimeFormat(video.currentTime) + " / " + convertToTimeFormat(video.duration);
     } else {
         timeElapsed.innerHTML = "00:00 / 00:00";
     }
-}, 500)
+    if (video.volume == 0) {
+        muteSvg.style.display = "block";
+        lowVolumeSvg.style.display = "none";
+        loudVolumeSvg.style.display = "none";
+    } else if (video.volume < 0.5) {
+        muteSvg.style.display = "none";
+        lowVolumeSvg.style.display = "block";
+        loudVolumeSvg.style.display = "none";
+    } else {
+        muteSvg.style.display = "none";
+        lowVolumeSvg.style.display = "none";
+        loudVolumeSvg.style.display = "block";
+    }
+}, 100)
 
-volumeWrapper.onmousedown  = e => {
-    const rect = e.target.getBoundingClientRect();
-    let volumePercentage = Math.round((e.clientX - rect.right));
-    volumeSlider.style.left = volumePercentage +"px";
-    video.volume = 1 - (volumePercentage * -0.01);
+
+for (let i = 0; i < video.textTracks.length; i++) {
+    video.textTracks[i].mode = "hidden";
+}
+
+captionsBtn.onmousedown = e => {
+    if (captions == null) {
+        alert("There are no captions available for this video");
+    } else {
+        const isHidden = captions.mode === "hidden";
+        captions.mode = isHidden ? "showing" : "hidden";
+    }
+}
+
+playbackBtn.onmousedown = e => {
+    let speed = video.playbackRate + 0.25;
+    if (speed > 2) speed = 0.25;
+    video.playbackRate = speed;
+    playbackBtn.innerHTML = `${speed}x`
+}
+
+pipBtn.onmousedown = e => {
+    togglePip();
+}
+
+
+fullscreenBtn.onmousedown = e => {
+    toggleFullscreen();
+}
+
+
+volumeBtn.onclick = e => {
+    if (isMute) {
+        video.volume = volume;
+        volumeSlider.value = volume * 100;
+        isMute = false;
+    } else {
+        volume = video.volume;
+        video.volume = 0;
+        volumeSlider.value = 0;
+        isMute = true;
+    }
+}
+
+volumeSlider.onchange = e => {
+    video.volume = volumeSlider.value * 0.01;
+}
+
+infoBtn.onmousedown = e => {
+    infoPopup.style.display = "block";
+}
+
+closeInfo.onmousedown = e => {
+    infoPopup.style.display = "none";
 }
